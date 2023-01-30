796c79ff447ca5d8c985c25e0e996b7148514702
Fredrik Lundström <70958591+frlundst@users.noreply.github.com>
2023-01-30 16:58:58 +0100

    Update App.tsx

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,6 +3,7 @@ import { Button } from '@mui/material'
 function App() {
   return (
     <div>
+      <h1>Test</h1>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
