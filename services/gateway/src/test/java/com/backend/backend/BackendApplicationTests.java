4d3e3965f382c53066af69131b86692d694ea090
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-02-02 14:34:57 +0100

    commit

--- services/gateway/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ services/gateway/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -2,13 +2,14 @@ package com.backend.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
+		Assert.isTrue(false, "Test failed");
 	}
 
 }
