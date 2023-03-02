ae9e82cc36c636327cf25101a2b05a7487df7810
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-03-02 17:52:14 +0100

    Update Component.tsx

--- webapp/src/Component/Views/Home/Component.tsx
+++ webapp/src/Component/Views/Home/Component.tsx
@@ -1,12 +1,23 @@
+import React from "react";
 import { ViewBaseProps } from "../../../Internalization/ViewBaseProps";
 import { useAuthStore } from "../../../Store/authStore";
 import { Person } from "../../../Types/Person";
+import { useNavigate } from "react-router-dom";
 
 export const Home: React.FC<ViewBaseProps> = ({ formatText }) => {
-    const person: Person = useAuthStore((state: any) => state.person);
+    const navigate = useNavigate();
+
+    const person: Person | null = useAuthStore((state: any) => state.person);
+
+    React.useEffect(() => {
+        if(!person){
+            navigate("/login");
+        }
+    }, []);
+
     return (
         <div>
-        <h1>{person.name}</h1>
+        <h1>Welcome {person?.name} {person?.surname}</h1>
         </div>
     );
 }
